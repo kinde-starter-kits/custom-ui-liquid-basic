68e5684f27a0c2d3247f01c5b4ec86c12cd63c95
Peter Phanouvong <peterphanouvong@gmail.com>
2025-03-12 14:45:19 +1100

    init

---
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -0,0 +1,50 @@
+"use server";
+import React from "react";
+import { renderToString } from "react-dom/server.browser";
+import { Liquid } from "liquidjs";
+
+export const pageSettings = {
+  bindings: {
+    url: {},
+  },
+};
+
+const LoginForm = () => {
+  return (
+    <html>
+      <div></div>
+    </html>
+  );
+};
+
+async function kindeRenderLiquid(people) {
+  const engine = new Liquid();
+  const tpl = engine.parse(`
+			<ul>
+			{%- for person in p %}
+			<li>
+				<a href="{{person | prepend: "https://example.com/"}}">
+				{{ person | capitalize }}
+				 <%!!! kindeLoginForm  !!!%>
+				</a>
+			</li>
+			{%- endfor%}
+			</ul>
+		`);
+  return engine.render(tpl, { p: people });
+}
+
+export default async function handleRequest(event: any) {
+  const paramsString = "q=URLUtils.searchParams&topic=api";
+  const searchParams = new URLSearchParams(paramsString);
+  // Iterating the search parameters
+  for (const p of searchParams) {
+    console.log(p);
+  }
+  const blah = LoginForm();
+  var heh = renderToString(blah);
+
+  const liquids = await kindeRenderLiquid(["John"]);
+
+  return heh + liquids;
+}
